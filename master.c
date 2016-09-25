b72d6270cfe154a1ae4ef7ea600069ad47647260
Brian Koehler <bekvff@umsl.edu>
2016-09-25 15:41:01 -0500

    now using SIGQUIT to kill processes on getpgrp(). Parent ignores kill signal to be able to continue to free up shared memory (not yet implemented)

--- master.c
+++ master.c
@@ -11,6 +11,7 @@
 //#include "logger.h"
 
 void alarmHandler(int);
+void processDestroyer(void);
 void printHelpMessage(void);
 void printShortHelpMessage(void);
 pid_t myPid, childPid;
@@ -105,11 +106,14 @@ int main (int argc, char **argv)
     return 0;
   }
 
-  //START PROCESS MANAGEMENT
-
+  //START PROCESS MANAGEMENT 
+  //
+  //Initialize the alarm handler
   signal(SIGALRM, alarmHandler);
+  //set the alarm to tValue seconds
   alarm(tValue);
 
+  //Fork sValue processes
   int j;
   for(j = 1; j <= sValue; j++) {
   
@@ -126,6 +130,7 @@ int main (int argc, char **argv)
     }
   }
 
+  //Wait for sValue number of processes to finish
   for(j = 1; j <= sValue; j++) {
     childPid = wait(&status);
     printf("Master: My child %d has died....\n", childPid);
@@ -134,28 +139,41 @@ int main (int argc, char **argv)
   return 0;
 }
 
+//alarm handler function that calls the process destroyer and the memory free-er
 void alarmHandler(int SIG){
-  signal(SIGALRM, SIG_IGN);
-  kill(-getpgrp(), SIGTERM);
-  sleep(2);
-  kill(-getpgrp(), SIGKILL);
+  processDestroyer();
 }
 
+//process destroyer. Parent ignores the SIGQUIT signal
+//kill calls SIGQUIT on the groupid to kill the children but
+//not the parent
+void processDestroyer() {
+  printf("processDestroyer() called\n");
+  signal(SIGQUIT, SIG_IGN);
+  kill(-getpgrp(), SIGQUIT);
+//  printf("Waiting 2 seconds before SIGKILL \n");
+//  sleep(2);
+//  signal(SIGINT, SIG_IGN);
+//  kill(-getpgrp(), SIGKILL);
+}
+
+//Long help message
 void printHelpMessage(void) {
     printf("\nThank you for using the help menu!\n");
     printf("The following is a helpful guide to enable you to use this\n");
     printf("slavedriver program to the best of your ability!\n\n");
     printf("-h, --help: Prints this help message.\n");
     printf("-s: Allows you to set the number of slave process to run.\n");
-    printf("\tThe default value is 5.\n");
+    printf("\tThe default value is 5. The max is 20.\n");
     printf("-l: Allows you to set the filename for the logger so the aliens can see how bad you mess up.\n");
     printf("\tThe default value is test.out.\n");
     printf("-i: Allows you to set the number of times each slave enters the critical section of code.\n");
     printf("\tThe default value is 3.\n");
-    printf("-t: Allows you set the wait time for the master process until it kills the slaves\n");
-    printf("\tThe default value is 20\n");
+    printf("-t: Allows you set the wait time for the master process until it kills the slaves.\n");
+    printf("\tThe default value is 20.\n");
 }
 
+//short help message
 void printShortHelpMessage(void) {
   printf("\nAcceptable options are:\n");
   printf("[-h], [--help], [-l][required_arg], [-s][required_arg], [-i][required_arg], [-t][required_arg]\n\n");
